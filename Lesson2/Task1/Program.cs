19cf4e85402dbec14f0e215319f0436626b1a625
Dek Igor <div_88@mail.ru>
2024-08-10 15:08:04 +0300

    Added Task1

---
+++ Lesson2/Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int[] arr = new int[2];
+arr[0] = 1;
+arr[1] = 5;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+
+int[] array = new int[5] { 3, 5, 6, 1, 7 };
+int[] array2 = { 3, 6, 9, 12 };
