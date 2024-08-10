babb38d9e5b8217e21f5eb4199b5b80458e20263
Dek Igor <div_88@mail.ru>
2024-08-10 16:00:53 +0300

    Added Task3

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// int n = 10;
+int[] array = { 2, 30, 15, 7, 3, 1, 6, 8, 0, 12 };
+int i = 0;
+while (i < array.Length)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
