dd22a0353ff04d90c6f5ea2583069f1263f4e336
Dek Igor <div_88@mail.ru>
2024-08-12 19:32:55 +0300

    Added Seminar02:Task2

---
+++ Seminar02/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число 
+// и возводит вторую цифру в степень, равную третьей цифре. Пример:
+
+// 487 => 8^7 = 2 097 158
+// 254 => 5^4 = 625
+
+
+System.Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if(number > 99 && number < 1000)
+{
+    int middleDigit = number / 10 % 10 ; // получаем двузначное целое число  и затем остаток от него (вторую цифру)
+    int rightDigit = number % 10; // получаем остаток от трёхзначного числа (третью цифру)
+    int power = 1;
+
+    for( ; rightDigit > 0; rightDigit--)
+    {
+        power *= middleDigit;
+    }
+    System.Console.WriteLine(power);
+}
+else
+{
+    System.Console.WriteLine("Число не трёхзначное!");
+}
+
