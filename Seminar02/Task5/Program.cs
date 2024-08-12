bd92cc59f7b53890cddb8bbc019969b6cd151160
Dek Igor <div_88@mail.ru>
2024-08-12 21:07:37 +0300

    Added Seminar02:Task5

---
+++ Seminar02/Task5/Program.cs
@@ -0,0 +1,22 @@
+﻿// Выполнить задание №4 только ищем третью цифру с начала 
+// Пример:
+
+// 456 => 6
+// 7812 => 1
+// 91 => Третьей цифры нет
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(System.Console.ReadLine());
+
+if (number > 99)
+{
+    while(number > 999)
+    {
+        number/= 10;
+    }
+    System.Console.WriteLine(number % 10);
+}
+else
+{
+    System.Console.WriteLine("Третьей цифры нет!");
+}
