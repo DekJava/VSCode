c92afc97994372b9cc26e84451620b367778f9da
Dek Igor <div_88@mail.ru>
2024-08-17 23:24:55 +0300

    Added HWSem02:Task4

---
+++ HWSem02/Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N, а на выходе 
+// показывает его цифры через запятую.
+
+System.Console.Write("Введите любое целое число: ");
+int number = Convert.ToInt32(System.Console.ReadLine());
+
+if(number < 0)
+{
+ System.Console.WriteLine(number);
+}
+else
+{
+    while(number > 0)
+    {
+        int currentDigit = number % 10;
+        number /= 10;
+    
+        if(number > 0)
+        {
+        System.Console.Write(currentDigit + ", ");
+        }
+        else
+        {
+        System.Console.Write(currentDigit);
+        }
+    }
+}
