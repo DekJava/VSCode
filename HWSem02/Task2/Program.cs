e9dfd313df10e352ea5f0f5e4ea99882594b8cc0
Dek Igor <div_88@mail.ru>
2024-08-29 19:50:31 +0300

    Readme added

--- HWSem02/Task2/Program.cs
+++ HWSem02/Task2/Program.cs
@@ -2,10 +2,14 @@
 // причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, 
 // в которой находится эта точка.
 
-System.Console.Write("Введите координату X: ");
-int X = Convert.ToInt32(System.Console.ReadLine());
-System.Console.Write("Введите координату Y: ");
-int Y = Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Введите координату X и Y через пробел: ");
+// int X = Convert.ToInt32(System.Console.ReadLine());
+// System.Console.Write("Введите координату Y: ");
+// int Y = Convert.ToInt32(System.Console.ReadLine());
+string[] coordinates = Console.ReadLine().Split(' ');
+
+int X = Convert.ToInt32(coordinates[0]);
+int Y = Convert.ToInt32(coordinates[1]);
 
 if (X > 0 && Y > 0)
 {
