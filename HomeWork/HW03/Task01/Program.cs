2c6d4dbfe29344ec60d71e5106352153058a3779
Dek Igor <div_88@mail.ru>
2024-09-03 20:14:17 +0300

    Added HW03 Task01

---
+++ HomeWork/HW03/Task01/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте массив целых чисел. Напишите программу, которая проверяет,
+// присутствует ли заданное число в массиве. Программа должна вывести:
+// Присутствует/Не присутствует.
+
+int[] numbers = {1, 3, 4, 19, 3};
+
+foreach(int number in numbers)
+{
+    System.Console.Write(number + " ");
+}
+
+System.Console.WriteLine();
+Console.Write("Введите искомое число: ");
+
+int numberToFind = Convert.ToInt32(Console.ReadLine());
+
+bool numberIsFinded = false;
+
+foreach(int number in numbers)
+{
+    if(number == numberToFind)
+        numberIsFinded = true;
+}
+
+if(numberIsFinded)
+{
+    System.Console.WriteLine("Число в массиве присутствует!");
+}
+else
+{
+System.Console.WriteLine("Числа в массиве нет!");
+}
