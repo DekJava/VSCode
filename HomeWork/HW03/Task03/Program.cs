68c6a33a84c49e08c484e9cb89410d9c50881b6f
Dek Igor <div_88@mail.ru>
2024-09-03 21:20:34 +0300

    Added HW03 Task03

---
+++ HomeWork/HW03/Task03/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте массив из вещественных чисел с ненулевой дробной
+// частью. Найдите разницу между максимальным и минимальным
+// элементом массива.
+
+double[] numbers = {2.86, 4.2, 5.9, 7.3, 9.1, 12.5, 35.28};
+
+double maxNumber = numbers[0];
+double minNumber = numbers[0];
+
+foreach(double number in numbers)
+{
+    if(number > maxNumber)
+    {
+        maxNumber = number;
+    }
+    else if(number < minNumber)
+    {
+        minNumber = number;
+    }
+}
+
+System.Console.WriteLine(maxNumber - minNumber);
