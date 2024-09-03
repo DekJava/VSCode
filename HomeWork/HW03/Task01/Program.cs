e905fd9b4a692ea42eb6fb9c490c3e71914c51ea
Dek Igor <div_88@mail.ru>
2024-09-03 20:42:23 +0300

    Changed Task01 in HW03

--- HomeWork/HW03/Task01/Program.cs
+++ HomeWork/HW03/Task01/Program.cs
@@ -1,32 +1,21 @@
-﻿// Задайте массив целых чисел. Напишите программу, которая проверяет,
-// присутствует ли заданное число в массиве. Программа должна вывести:
-// Присутствует/Не присутствует.
+﻿// Задайте массив на 10 целых чисел. Напишите программу
+// которая определяет количество чётных чисел в массиве
+// Пример int[] numbers = {3, 1, 6, 6, 3, 6, 2, 6} => 5
 
-int[] numbers = {1, 3, 4, 19, 3};
+int[] numbers = {2, 6, 1, 7, 4, 7, 9, 34, 43, 32};
+
+int sum = 0;
+
+foreach(int number in numbers)
+{
+    if(number % 2 == 0)
+    {
+        sum += 1;
+    }
+}
 
 foreach(int number in numbers)
 {
     System.Console.Write(number + " ");
 }
-
-System.Console.WriteLine();
-Console.Write("Введите искомое число: ");
-
-int numberToFind = Convert.ToInt32(Console.ReadLine());
-
-bool numberIsFinded = false;
-
-foreach(int number in numbers)
-{
-    if(number == numberToFind)
-        numberIsFinded = true;
-}
-
-if(numberIsFinded)
-{
-    System.Console.WriteLine("Число в массиве присутствует!");
-}
-else
-{
-System.Console.WriteLine("Числа в массиве нет!");
-}
+System.Console.WriteLine(" => " + sum);
