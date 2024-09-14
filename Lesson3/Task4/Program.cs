225114d54a54973812db6029614dc29f816e3232
Dek Igor <div_88@mail.ru>
2024-09-14 17:34:29 +0300

    Task4 Lesson3

---
+++ Lesson3/Task4/Program.cs
@@ -0,0 +1,35 @@
+﻿// Создаем генерацию рандомных чисел и ввод с клавиатуры
+
+Random rnd = new Random();
+
+int size = 30;
+int[] arr = new int[size];
+int i = 0;
+while (i < size)
+{
+    arr[i] = rnd.Next(1, 10);
+    i++;
+}
+
+i = 0;
+while(i < size)
+{
+    Console.Write($"{arr[i]} ");
+    i++;
+}
+Console.WriteLine(" ");
+
+// Введение чисел с клавиатуры
+
+int sizeIn = 7;
+int[] array = new int[sizeIn];
+int j = 0;
+
+while(j < sizeIn)
+{
+Console.WriteLine("Введите число массива: ");
+string input = Console.ReadLine();
+array[i] = Convert.ToInt32(input);
+j++;
+}
+
