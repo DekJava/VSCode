ede9a9e39ec80448912fef4af159a4ad18363169
Dek Igor <div_88@mail.ru>
2024-09-14 17:27:42 +0300

    Desided Task03 in HW04 Functions

---
+++ Seminars/Sem04_Functions/HomeWork/Task03/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 3: 
+
+// Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, 
+// второй – предпоследним и т.д.)
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        void Main()
+        {
+            Random rnd = new Random();
+            int[] rndArray = new int[10];
+
+                System.Console.Write("Массив до: ");
+            for(int i = 0; i < rndArray.Length; i++)
+            {
+                int array = rndArray[i] = rnd.Next(1, 20);
+                System.Console.Write(array + " ");
+
+            }
+
+            for(int i = 0; i < rndArray.Length / 2; i++)
+            {
+                int temp = rndArray[i];
+                rndArray[i] = rndArray[rndArray.Length - 1 - i];
+                rndArray[rndArray.Length - 1 - i] = temp;
+            }
+            System.Console.WriteLine();
+            System.Console.Write("После    : ");
+            for(int i = 0; i < rndArray.Length; i++)
+        {
+            System.Console.Write(rndArray[i] + " ");
+        }
+
+        }
+
+        Main();
+        
+    }
+}
