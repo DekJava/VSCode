92a5ba354b6d3879cf10aa813664450988325345
Dek Igor <div_88@mail.ru>
2024-09-15 17:37:10 +0300

    Added Task01 Arrays2

---
+++ Seminars/Sem05_Arrays2/IndepWork/Task01/Program.cs
@@ -0,0 +1,43 @@
+﻿// Двумерные массивы. Лекция
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        void Main()
+        {
+            int[,] matrix2 = CreateMatrix(4, 12);
+            PrintMatrix(matrix2);
+        }
+
+    int[,] CreateMatrix(int row, int column)
+    {
+        int[,] matrix = new int[row, column];
+        Random rnd = new Random();
+
+        for(int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for(int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] = rnd.Next(1, 11);
+            }
+        }
+        return matrix;
+    }
+
+    void PrintMatrix(int[,] matrix)
+    {
+        for(int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for(int j = 0; j < matrix.GetLength(1); j++)
+            {
+                System.Console.Write($"{matrix[i, j]} ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+
+    Main();
+    }
+
+}
